f1b2225c557ad78f009c67ac0472bdd6adb70390
chaewon914 <61789181+chaewon914@users.noreply.github.com>
2020-04-21 23:45:03 +0900

    mise

--- app.py
+++ app.py
@@ -56,12 +56,14 @@ def api_signup():
    cold_receive = request.form['cold_give']
    wind_receive = request.form['wind_give']
    email_receive = request.form['email_give']
+   address_receive = request.form['address_give']
 
    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
 
    db.user.insert_one({'id':id_receive,'pw':pw_hash,'nick':nickname_receive,  'rain': rain_receive, 'mise': mise_receive, 'heat': heat_receive, 'cold': cold_receive,
-   'wind': wind_receive, 'email': email_receive
+   'wind': wind_receive, 'email': email_receive, 'address': address_receive
+
    })
 
    return jsonify({'result': 'success'})
